44a3120cd620a497947200cb2241a600e4ba00aa
Chris Nyaga <nyagachris75@gmail.com>
2017-11-21 15:36:07 +0300

    Moved the advanced search to the user model.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,38 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
-use Illuminate\Support\Facades\DB;
+
 
 class PagesController extends Controller{
     public function home(Request $request){
       // TODO: Move this search to the User model.
-        $q = $request->get('q');
-        if ($q) {
-            $users = User::search($q)->paginate(5);
-            return view('index', compact('users', 'q'));
-        }
-        $name = $request->get('name') ?? "" ?: null;
-        $company = $request->get('company') ?? "" ?: null;
-        $title = $request->get('title') ?? "" ?: null;
-        $yearStart = $request->get('year-start') ?? "" ?: null;
-        $yearEnd = $request->get('year-end') ?? "" ?: null;
-        $users = User
-            ::select(DB::raw("*"))
-            ->when($name, function ($query) use ($name) {
-                return $query->where('name', 'LIKE', '%' . $name . '%');
-            })
-            ->when($company, function ($query) use ($company) {
-                return $query->where('company', 'LIKE', '%' . $company . '%');
-            })
-            ->when($title, function ($query) use ($title) {
-                return $query->where('title', 'LIKE', '%' . $title . '%');
-            })
-            ->when($yearStart, function ($query) use ($yearStart) {
-                return $query->where('year-start', '=', $yearStart);
-            })
-            ->when($yearEnd, function ($query) use ($yearEnd) {
-                return $query->where('year-end', '=', $yearEnd);
-            })->paginate(5);
+
+      // Simle query
+      $q = $request->get('q');
+      if ($q) {
+          $users = User::search($q)->paginate(5);
+          return view('index', compact('users', 'q'));
+      }
+      // Try and advanced search or else return all users (paginated)
+      $users = User::advancedSearch($request)->paginate(5);
       return view('index', compact('users', 'name', 'company', 'title', 'yearStart', 'yearEnd'));
     }
 }
